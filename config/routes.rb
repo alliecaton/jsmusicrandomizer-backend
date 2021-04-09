f97e90505e8a183237c1ab06f4d9df344abc6ad0
Allie <alliecaton3@gmail.com>
2021-04-09 10:14:47 -0400

    changed routes to route to name rather than id

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :songs
-  resources :users
+  resources :users, param: :name
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
