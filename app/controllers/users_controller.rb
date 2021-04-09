0f1ea8cc874be1a76aa6d11744b3842a29713147
Allie <alliecaton3@gmail.com>
2021-04-09 10:21:48 -0400

    changed params tot ake in name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.find_by(name: params[:name])
     end
 
     # Only allow a list of trusted parameters through.
